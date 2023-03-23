240e2d7acc3df4a803d1007065a9fef915cc4767
KawaiiPGR <36188023+Kira-Pgr@users.noreply.github.com>
2023-03-24 00:31:32 +0800

    Update train_rm.py

--- train_rm.py
+++ train_rm.py
@@ -118,9 +118,8 @@ def train(args):
     trainer.fit()
     # save model checkpoint after fitting on only rank0
     #model_without_ddp = trainer.model.module
-    #strategy.save_model(model_without_ddp, args.save_path, only_rank0=True)
-    if strategy.get_rank() == 0:
-        torch.save(model.state_dict(), args.save_path)
+    strategy.save_model(trainer.model, args.save_path, only_rank0=True)
+    #torch.save(model.state_dict(), args.save_path)
     # save optimizer checkpoint on all ranks
     if args.need_optim_ckpt:
         strategy.save_optimizer(trainer.optimizer, 'rm_optim_checkpoint_%d.pt' % (torch.cuda.current_device()), only_rank0=False)
